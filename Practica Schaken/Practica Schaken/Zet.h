477858daa4a751a59acf509a258a1768b344a364
Jecey <jeroen-ceyssens2@hotmail.com>
2015-04-13 14:06:06 +0200

    Made some crazy shit testers in zet

--- Practica Schaken/Practica Schaken/Zet.h
+++ Practica Schaken/Practica Schaken/Zet.h
@@ -1,10 +1,19 @@
 #ifndef ZET_H
-#def ZET_H
-#include "Spel.h"
+#define ZET_H
+class Spelbord;
+class Pion;
 class Zet
 {
 	public:
-		void moveTo(Spelbord &spelbord, Pion &pion, int coX, int coY);
+		void moveTo(Spelbord &spelbord, Pion &pion, int toX, int toY);
+		//booleans that check which moves the pions can do
+		bool canHorseMove(int fromX, int fromY, int toX, int toY);
+		bool canSoldMove(int fromX, int fromY, int toX, int toY);
+		bool canCastleMove(int fromX, int fromY, int toX, int toY);
+		bool canBissMove(int fromX, int fromY, int toX, int toY);
+		bool canQueenMove(int fromX, int fromY, int toX, int toY);
+		bool canKingMove(int fromX, int fromY, int toX, int toY);
+
 	private:
 		
 };
